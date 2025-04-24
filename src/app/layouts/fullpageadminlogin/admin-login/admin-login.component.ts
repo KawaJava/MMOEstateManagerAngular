cabc3be3eef7188456d8d3c1d831c14eda2e5b50
kawajava <kawajava0550@gmail.com>
2025-04-24 12:16:27 +0200

    JWT service added

--- src/app/layouts/fullpageadminlogin/admin-login/admin-login.component.ts
+++ src/app/layouts/fullpageadminlogin/admin-login/admin-login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminLoginService } from '../admin-login.service';
+import { Router } from '@angular/router';
+import { JwtService } from 'src/app/modules/common/service/jwt.service';
 
 @Component({
   selector: 'app-admin-login',
@@ -9,14 +12,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AdminLoginComponent implements OnInit {
 
   formGroup!: FormGroup;
+  loginError = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private adminLoginService: AdminLoginService,
+    private router: Router,
+    private jwtService: JwtService
+  ) { }
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     })
+  }
+
+  submit(){
+    if(this.formGroup.valid){
+      this.adminLoginService.login(this.formGroup.value)
+        .subscribe({
+          next: (response) => {
+            this.loginError = false;
+            if(true){
+              this.jwtService.setToken(response.token);
+              // this.jwtService.setAdminAccess(true);
+            }
+            this.router.navigate(["/admin"]);
+          },
+          error: () => this.loginError = true
+        })
+    }
   }
 
 }
