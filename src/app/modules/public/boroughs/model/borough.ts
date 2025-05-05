652c969cac915c2b3830725f4fca6c9347f147d0
kawajava <kawajava0550@gmail.com>
2025-05-05 14:24:09 +0200

    Added leaders name to boroughs

--- src/app/modules/public/boroughs/model/borough.ts
+++ src/app/modules/public/boroughs/model/borough.ts
@@ -1,12 +1,14 @@
+import { Player } from "../../players/model/player";
+
 export interface Borough {
     id: number;
     name: string;
     slug: string;
     countryId: number;
-    actualLeaderId: number;
+    actualLeader: Player;
     leaderStartDate: string;
     actualGold: string;
-    goldAddedBy: number;
+    goldAddedBy: Player;
     dateAdded: string;
     emailSend: boolean;
 }
